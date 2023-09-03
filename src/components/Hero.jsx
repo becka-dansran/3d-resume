ff046862e6279413cd9352efb72ce41076a923ac
test <test@Becks-MacBook-Pro.local>
2023-09-02 21:48:38 -0500

    name: naran J

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
         {/* col 2 */}
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915eff]">Naran</span>
+            Hi, I'm <span className="text-[#915eff]">Naran J</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I'm passionate about building user interfaces with React JS.
